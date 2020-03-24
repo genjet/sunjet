146bea37d0f36359b5fdcd23d1e0b88b00f00ee1
AndyWang <andywang@bluetechnology.com.tw>
2020-03-24 18:01:18 +0800

    test 1

--- sunjet-root/sunjet-front/src/main/java/com/sunjet/front/configuration/WebMvcConfiguration.java
+++ sunjet-root/sunjet-front/src/main/java/com/sunjet/front/configuration/WebMvcConfiguration.java
@@ -1,13 +1,17 @@
 package com.sunjet.front.configuration;
 
+import java.util.concurrent.TimeUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.WebJarsResourceResolver;
 
 import com.sunjet.front.interceptor.PlatformInterceptor;
 
@@ -26,10 +30,21 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 		registry.addInterceptor(platformInterceptor).addPathPatterns("/**").excludePathPatterns(patterns);
 	}
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/static/**", "/webjars/**").addResourceLocations("classpath:/static/", "classpath:/META-INF/resources/webjars/");
-	}
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		registry.addResourceHandler("/static/**", "/webjars/**").addResourceLocations("classpath:/static/", "/resources/", "/webjars/");
+//	}
+	
+	 @Override
+	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	        registry.addResourceHandler("/static/**", "/webjars/**")
+	                .addResourceLocations("classpath:/static/", "/resources/", "/webjars/")
+	                .setCacheControl(
+	                        CacheControl.maxAge(30L, TimeUnit.DAYS).cachePublic())
+	                .resourceChain(true)
+	                .addResolver(new WebJarsResourceResolver());
+
+	    }
 
 	// @Override
 	// public void addCorsMappings(CorsRegistry registry) {
